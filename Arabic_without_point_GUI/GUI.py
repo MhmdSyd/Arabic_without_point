66017ec96c3fe29515f2b6bca573e4f0e5caf49c
mhmdsyd <mhmdsyd.workout@gmail.com>
2021-05-18 00:11:41 +0200

    Noun edit

--- Arabic_without_point_GUI/GUI.py
+++ Arabic_without_point_GUI/GUI.py
@@ -17,18 +17,25 @@ def readJson():
 def transform():
     # read text from text box 1.
     inputValue = t1.get("1.0",'end-1c') 
+    inputValue = inputValue + " "
     # clear content from textbox2.
     t2.delete("1.0",END) 
     # read json dictionary and store it in mydict.
     mydict = readJson()
     # loop for all letter to check that is letter is in dictionary to transform it or not.
-    for i in inputValue:
-        if i in mydict.keys():
+    for i in range(len(inputValue)):
+        # check where letter noun in word to decided if i replace it or not 
+        if inputValue[i] == noun.decode("utf-8") and (inputValue[i+1] != " " and inputValue[i+1] != "\n"):
+            y = baq.decode("utf-8")
+        else:
+            y = inputValue[i]
+
+        if y in mydict.keys():
             # True condition: transform letter.
-            t2.insert(INSERT,mydict[i])
+            t2.insert(INSERT,mydict[y])
         else:
             # False condition: don't transform.
-            t2.insert(INSERT,i)
+            t2.insert(INSERT,y)
 # this method use to clear textbox1 then paste text which you copy in textbox1.
 def pasteText():
     t1.delete("1.0",END)
@@ -45,19 +52,22 @@ def reset():
 
 # start of main code.
 if __name__ == "__main__":
+    # letter baq and noun encoding in utf-8
+    noun = b'\xd9\x86'
+    baq = b'\xd8\xa8'
     # create frame of window GUI
     window = Tk()
     window.title("Arabic_without_point")
     window.geometry("600x300")
     window.resizable(width=False,height=False)
     # textbox1 use to put text which you need to transform.
-    t1 = Text(window, width=30,height=15)
+    t1 = Text(window, width=32,height=15)
     t1.grid(row=1,column=1)
     # this button use to transform text to Arabic without dots.
     b1 = Button(window,text="submit",width=8, height=2, command=lambda: transform())
     b1.grid(row=1, column=2)
     # textbox2 is use to print and show text after transform.
-    t2 = Text(window, width=30,height=15)
+    t2 = Text(window, width=32,height=15)
     t2.grid(row=1,column=3)
     # buttton use to paste the content which you want to transform.
     b2 = Button(window,text="paste",width=8, height=2, command=lambda: pasteText())
@@ -65,7 +75,7 @@ if __name__ == "__main__":
     # butto that copy text after transform.
     b3 = Button(window,text="copy",width=8, height=2, command=lambda: copyText())
     b3.grid(row=2, column=3)
-
+    # butto that clear text from textbox1 and textbox2.
     b3 = Button(window,text="reset",width=8, height=2, command=lambda: reset())
     b3.grid(row=2, column=2)
 
--- arabic_without_point.py
+++ arabic_without_point.py
@@ -4,7 +4,9 @@ import json
 
 # change directory to project file location.
 os.chdir(os.path.dirname(__file__))
-
+# letter baq and noun encoding in utf-8
+noun = b'\xd9\x86'
+baq = b'\xd8\xa8'
 # read transform letter Dictionary.
 myfile = open("transform_dict.json", "r", encoding="utf-8")
 mydic = json.load(myfile)[0]
@@ -13,21 +15,29 @@ myfile.close()
 # read text content that you wish to transform.
 myfile = open("input.txt", "r", encoding="utf-8")
 lines = myfile.readlines()
+lines[-1] = lines[-1]+" "
 myfile.close()
 
 # open output file to write the transform text.
 myfile = open("output.txt", "w", encoding="utf-8")
 # loop for all lines that read form input file.
 for i in lines:
+    i = i + " "
+    letterList =[]
     # loop for all letter in lines.
     for j in range(len(i)):
+        if i[j] == noun.decode("utf-8") and (i[j+1] != " " and i[j+1] != "\n"):
+            y = baq.decode("utf-8")
+        else:
+            y = i[j]
         # check that letter is a key in dictionary.
-        if i[j] in mydic.keys():
+        if y in mydic.keys():
             # True condition: replace letter by the value in dict and write letter in output file..
-            myfile.write(mydic[i[j]])
+            letterList.append(mydic[y])
         else:
             # False condition: don't replace and write letter in output file.
-            myfile.write(i[j])
+            letterList.append(y)
+    myfile.write("".join(letterList).strip(" "))
 
 myfile.close()
 
